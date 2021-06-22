f2524ac5328e5409672542dbcbbd900e4131ef52
hizashi <zytwp@hotmail.com>
2021-06-22 14:19:49 +0800

    added unique for name on dailies.model, added post route on admin for dailies

--- models/dailies.model.js
+++ models/dailies.model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const dailiesSchema = new Schema({
     category: {type: String},
-    name: {type:String}
+    name: {type:String, unique: true}
 })
 
 module.exports = mongoose.model('Daily', dailiesSchema)
