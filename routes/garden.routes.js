f61cf7c8057f972fe5481a9316669f0571cef378
yjrandom <random.ran.dom@hotmail.com>
2021-06-24 15:43:11 +0800

    Creates garden model at garden component, deletes inventory

--- routes/garden.routes.js
+++ routes/garden.routes.js
@@ -3,12 +3,24 @@ const PlantModel = require('../models/plant.model')
 const GardenModel = require('../models/garden.model')
 const UserModel = require('../models/user.model')
 const checkUser = require('../lib/checkUser')
+const {removeItemFromArray} = require("../lib/func")
 
 // Get garden of user
 router.get("/", checkUser, async (req,res) => {
     try {
         let userId = req.user.id
-        let garden = await GardenModel.findOne({user: userId}).populate("plants")
+        let garden = await GardenModel.findOne({user: userId})
+        if(!garden) {
+            let newGarden = new GardenModel({user: userId})
+            await newGarden.save()
+            garden = await GardenModel.findOne({user: userId}).populate("plants")
+        }
+
+        for(let i = 0; i < garden.plants.length; i++) {
+            let plant =  await PlantModel.findById(garden.plants[i])
+            garden.plants[i] = plant
+        }
+
         res.status(200).json({garden})
     } catch (e) {
         res.status(400).json({message: "Failed to get inventory"})
@@ -30,6 +42,9 @@ router.get("/inventory", checkUser, async (req,res) => {
 router.post("/inventory/:id", checkUser, async (req, res) => {
     try {
         let userId = req.user.id
+        let user = await UserModel.findById(userId)
+        let userPlants = user.plants
+        await UserModel.findByIdAndUpdate(userId, {plants: removeItemFromArray(req.params.id, userPlants)}, {new: true})
         let tempGarden = await GardenModel.findOne({user: userId})
         tempGarden.plants[req.body.index] = req.params.id
         let plants = tempGarden.plants
